bbcde6481b9ddc7d833ecb0d4358c30ceb19146e
challa143143 <91412677+challa143143@users.noreply.github.com>
2022-04-06 05:08:03 +0000

    Add files via upload

---
+++ que1.ps1
@@ -0,0 +1,11 @@
+﻿write-host ("DINESH NAIDU") -ForegroundColor yellow 
+ #importing active Directory module  
+ Import-Module ActiveDirectory  
+ #giving name to a user  
+ $AccountName=read-host("enter the user name") 
+ # adding user  
+ New-ADUser $AccountName  
+ #displaying users  
+ write-host("displaying all the created/exixting user")  
+ Get-ADUser -Filter * -Properties samAccountName | select samAccountName 
+ write-host("user added successfully") 
